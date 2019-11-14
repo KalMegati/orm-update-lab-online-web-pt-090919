96a62dc5ac73ee1cf7a10e5cd2e4949df368f542
Srikalyan Megati <kalmegati@gmail.com>
2019-11-14 14:38:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,13 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
-  :attr_accessor
+  attr_accessor :id, :name, :grade
+  
+  def initialization(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
 
 end
